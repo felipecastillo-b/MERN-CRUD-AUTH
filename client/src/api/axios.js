02d0892da575ab1ccd52b8f920c4cab6e3c60d94
Felipe Castillo <felipecastillo.snk@gmail.com>
2023-12-23 16:54:27 -0300

    cyclic backen prueba

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
     //baseURL: 'http://localhost:4000/api',
-    baseURL: 'https://mern-crud-auth.onrender.com/',
+    //baseURL: 'https://mern-crud-auth.onrender.com/',
+    baseURL: 'https://awful-cow-jacket.cyclic.app',
     withCredentials: true,
 });
 
