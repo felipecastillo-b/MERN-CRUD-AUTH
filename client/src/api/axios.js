f135c427aed7f1a1753c1de455eba20cddeb2575
Felipe Castillo <felipecastillo.snk@gmail.com>
2023-12-23 17:34:27 -0300

    fix api

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     //baseURL: 'http://localhost:4000/api',
     //baseURL: 'https://mern-crud-auth.onrender.com/',
-    baseURL: 'https://awful-cow-jacket.cyclic.app/',
+    baseURL: 'https://awful-cow-jacket.cyclic.app/api',
     withCredentials: true,
 });
 
