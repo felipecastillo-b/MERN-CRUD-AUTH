fff8a64aa99148b5711b95d77373f57327311300
Felipe Castillo <felipecastillo.snk@gmail.com>
2023-12-23 14:25:29 -0300

    fixed

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api", authRoutes);
-app.use("/api", taskRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
 
 export default app;
