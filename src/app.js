e549fa0c152ce3e6430178f08c27c35e6677623b
Felipe Castillo <felipecastillo.snk@gmail.com>
2023-12-23 13:38:03 -0300

    fixed appuse

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-import authRoutes from "./routes/auth.routes.js"
-import taskRoutes from "./routes/tasks.routes.js"
+import authRoutes from "./routes/auth.routes.js";
+import taskRoutes from "./routes/tasks.routes.js";
 
-const app = express()
+const app = express();
 
 app.use(cors({
     origin: ['http://localhost:5173', 'https://mern-crud-auth-front.onrender.com'],
@@ -16,7 +16,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api", authRoutes);
-app.use("/api", taskRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
 
 export default app;
