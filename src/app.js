fc704919b46bdb6994a0bae152f4dcf495fd9534
Felipe Castillo <felipecastillo.snk@gmail.com>
2023-12-23 18:21:39 -0300

    cookie fix 1821

--- src/app.js
+++ src/app.js
@@ -8,15 +8,6 @@ import taskRoutes from "./routes/tasks.routes.js";
 
 const app = express();
 
-// Middleware para manejar encabezados CORS
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://mern-crud-auth-front.onrender.com');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
-
 app.use(cors({
     //origin: 'http://localhost:5173',
     origin: 'https://mern-crud-auth-front.onrender.com',
