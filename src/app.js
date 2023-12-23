a843fcc7fe18ab72ac6ca6672171ccc933fc9f5f
Felipe Castillo <felipecastillo.snk@gmail.com>
2023-12-23 13:10:33 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import taskRoutes from "./routes/tasks.routes.js"
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://mern-crud-auth-front.onrender.com'],
     credentials: true
 }));
 app.use(morgan("dev"));
