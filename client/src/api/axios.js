f204a64bc089877a1ad90149ad6e6e54f93e3d9c
Felipe Castillo <felipecastillo.snk@gmail.com>
2023-12-23 13:21:55 -0300

    fixed cors

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: ['http://localhost:4000/api', 'https://mern-crud-auth-front.onrender.com'],
+    //baseURL: 'http://localhost:4000/api',
+    baseURL: 'https://mern-crud-auth-front.onrender.com',
     withCredentials: true,
 });
 
